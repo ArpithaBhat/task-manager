6a2616211b674d89d673d873edede5a90ecca951
AB <arpi.g8@gmail.com>
2025-08-02 17:24:01 -0600

    Checking for CI TRigger

--- test/test_task_manager.py
+++ test/test_task_manager.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 # Add the parent directory to the path
+# check if github ci triggered
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Services.task_manager import TaskManager
